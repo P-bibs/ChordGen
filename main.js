95462345ac6b57c509c6426977c5fc14fb81e423
Paul Biberstein <biberstein.paul@gmail.com>
2019-05-09 12:42:55 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,102 @@
+var model;
+var hashedReverseDict;
+var synth;
+var lastPrediction;
+var doStopPlayback;
+var MINIMUM_CHORDAL_MEMBERS = 3;
+
+async function initModel(){
+  model = await tf.loadLayersModel('http://localhost:8081/model.json');
+  hashedReverseDict = JSON.parse(Get('http://localhost:8081/hashedReverseDict.json'));
+}
+
+function generateProg(){
+  //TODO: load model and data
+  var data = lastPrediction || myProg;
+
+  var prediction = model.predict(tf.tensor([data]));
+  prog = shapeModelOutput(prediction);
+  lastPrediction = prog
+  playProg(prog);
+}
+
+function shapeModelOutput(raw){
+  //turn tensor into array (blocking)
+  // TODO: make async
+  var arr = raw.as2D(5,12).dataSync();
+
+  //reshape array
+  var out = []
+  for (i=0; i<5; i+=1) {
+    out[i] = arr.slice(12*i, 12*(i+1))
+  }
+
+  //out = probabilityToBinary(out);
+
+  out = out.map(function(a){
+    return a.map(function(b){
+      return b > .5 ? 1 : 0
+    })
+  })
+
+  return out;
+}
+
+function probabilityToBinary(probArry) {
+  var binary = [[], [], [], [], []];
+  //convert to binary
+
+  for (var i = 0; i<5; i+=1) {
+    for (var thresh = .5; thresh < 1; thresh += .1) {
+      for (var j = 0; j<12; j+=1) {
+        binary[i][j] = probArry[i][j] > thresh ? 1 : 0;
+      }
+      // Count number of '1's in array. If greater than min,
+      // then stop increasing threshold and continue.
+      if (binary[i].reduce( (a,b) => a+b ) >= MINIMUM_CHORDAL_MEMBERS){
+        break;
+      }
+    }
+  }
+
+  return binary;
+}
+
+function playProg(prog){
+  if (prog.length === 0) {
+    document.getElementById("chordNotesDisplay").innerHTML = "Chord notes will be displayed here";
+    document.getElementById("chordNameDisplay").innerHTML = "Chord name will be displayed here";
+    return;
+  }
+  if (doStopPlayback){
+    doStopPlayback = false;
+    return;
+  }
+
+  synth = new Tone.PolySynth(5, Tone.synth).toMaster();
+
+  var notes = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4"]
+  var chord = mask(notes, prog[0]);
+  
+  synth.triggerAttack(chord);
+
+  setTimeout(function(){
+    document.getElementById("chordNotesDisplay").innerHTML = prog[0];
+    document.getElementById("chordNameDisplay").innerHTML = hashedReverseDict[getPseudoHash(prog[0])] || "<unk>";
+    synth.triggerRelease(chord);
+    playProg(prog.slice(1, prog.length));
+  }, 1000);
+}
+
+function stopPlayback() {
+  doStopPlayback = true;
+  synth.releaseAll();
+}
+
+var myProg = [
+  [1,0,0,0,0,0,0,1,0,0,0,0],
+  [1,0,0,0,1,0,0,1,0,1,0,0],
+  [1,0,0,0,0,0,0,1,0,1,0,0],
+  [1,0,1,0,0,1,0,1,0,1,0,0],
+  [0,0,1,0,0,1,0,1,0,0,0,1]
+]
---
+++ util.js
@@ -0,0 +1,25 @@
+function Get(yourUrl){
+  var Httpreq = new XMLHttpRequest(); // a new request
+  Httpreq.open("GET",yourUrl,false);
+  Httpreq.send(null);
+  return Httpreq.responseText;          
+}
+
+function mask (data, mask) {
+  return data.filter((item, i) => mask[i]);
+}
+
+function getPseudoHash(arry){
+  hash = ""
+  for (var i = 0; i < 12; i+=3) {
+    seg = arry.slice(i, i+3).reduce(function(a,b){
+      return a.toString() + b.toString();
+    })
+    hash += String.fromCharCode(97 + parseInt( seg, 2 ));
+  }
+  return hash
+}
+
+function temp() {
+  getPseudoHash([1,0,0,0,0,1,0,0,0,0,1,1])
+}
