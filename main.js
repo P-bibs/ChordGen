917901cd00875cb96d7a47f483609753b552e965
Paul Biberstein <biberstein.paul@gmail.com>
2019-05-20 12:03:41 -0400

    Added display for full progression

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var hashedReverseDict;
 var synth;
 
 var currentProg = [];
-var seed = []
+var currentProgMembers = [];
+var currentProgNames = [];
+var seed = [];
 var currentChordIndex;
 var playbackTimer;
 
@@ -12,14 +14,38 @@ var settings = {
   chordsPerProg: 5,
   minimumChordalMembers: 3,
   noteDuration: 2,
-  loopPlayback: false
+  loopPlayback: false,
+  onlyNamedChords: true
 }
 
 async function init(){
   model = await tf.loadLayersModel('http://localhost:8081/model.json');
   hashedReverseDict = JSON.parse(Get('http://localhost:8081/hashedReverseDict.json'));
   seed = generateRandomProg(5, settings.minimumChordalMembers);
+  addConfigEventListeners();
   generateProg();
+}
+
+function addConfigEventListeners(){
+  document.getElementById("chordsPerProg").addEventListener("input", function(a){
+    settings.chordsPerProg = this.value
+  });
+
+  document.getElementById("minimumChordalMembers").addEventListener("input", function(a){
+    settings.minimumChordalMembers = this.value
+  });
+  document.getElementById("noteDuration").addEventListener("input", function(a){
+    settings.noteDuration = this.value
+  });
+  // document.getElementById("keySelect").addEventListener("input", function(a){
+  //   settings.key = this.value
+  // });
+  document.getElementById("loopPlayback").addEventListener("change", function(a){
+    settings.loopPlayback = document.getElementById("loopPlayback").checked;
+  });
+  document.getElementById("onlyNamedChords").addEventListener("change", function(a){
+    settings.onlyNamedChords = document.getElementById("onlyNamedChords").checked;
+  });
 }
 
 function generateProg(){
@@ -39,7 +65,14 @@ function generateProg(){
     newProg = newProg.slice(0, settings.chordsPerProg);
   }
   currentProg = newProg;
+  determineChordalMembersAndNames();
+  if (currentProgNames.includes("<unk>") && settings.onlyNamedChords) {
+    generateProg();
+    return;
+  }
   currentChordIndex = 0;
+
+  writeProgToHtml();
 }
 
 function shapeModelOutput(raw){
@@ -57,12 +90,12 @@ function shapeModelOutput(raw){
 
 function probabilityToBinary(probArry) {
   var binary = [];
-  for (var i = 0; i < 5; i+=1) {
+  for (let i = 0; i < 5; i+=1) {
     binary[i] = [];
   }
 
   //convert to binary
-  for (var i = 0; i<5; i+=1) {
+  for (let i = 0; i<5; i+=1) {
     for (var thresh = .5; thresh > 0; thresh -= .1) {
       for (var j = 0; j<12; j+=1) {
         binary[i][j] = probArry[i][j] > thresh ? 1 : 0;
@@ -82,7 +115,7 @@ function playProg(){
   stopPlayback();
 
   voiceCount = 0;
-  for (var i = 0; i < currentProg.length; i+=1) {
+  for (let i = 0; i < currentProg.length; i+=1) {
     if (currentProg[i].reduce((a,b) => a+b) > voiceCount) {
       voiceCount = currentProg[i].reduce((a,b) => a+b);
     }
@@ -108,24 +141,8 @@ function playCurrentChord() {
     return;
   }
 
-  var notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
-  var chord = mask(notes, currentProg[currentChordIndex]);
-
-  console.log("attack triggered: " + chord + " " + currentChordIndex);
-  synth.triggerAttack(chord.map(a => a + "4"));
-
-  document.getElementById("chordMembers0").innerHTML = chord.map(a => a+"<br>").toString().replace(/,/g, "");
-  document.getElementById("chordName0").innerHTML = hashedReverseDict[getPseudoHash(currentProg[currentChordIndex])] || "<unk>";
-  
-  if (currentChordIndex + 1 !== settings.chordsPerProg) {
-    var nextChord = mask(notes, currentProg[currentChordIndex + 1]) || "<end>";
-    document.getElementById("chordMembers1").innerHTML = nextChord.map(a => a+"<br>").toString().replace(/,/g, "");
-    document.getElementById("chordName1").innerHTML = hashedReverseDict[getPseudoHash(currentProg[currentChordIndex+1])] || "<unk>";
-  }
-  else {
-    document.getElementById("chordMembers1").innerHTML = ""
-    document.getElementById("chordName1").innerHTML = ""
-  }
+  console.log("attack triggered: " + currentProgMembers[currentChordIndex] + " " + currentChordIndex);
+  synth.triggerAttack(currentProgMembers[currentChordIndex].map(a => a + "4"));
 }
 
 function stopPlayback() {
@@ -134,19 +151,82 @@ function stopPlayback() {
 
     currentChordIndex = 0;
 
-    document.getElementById("chordMembers0").innerHTML = "Chord members 1 here";
-    document.getElementById("chordMembers1").innerHTML = "Chord members 2 here";
-    document.getElementById("chordName0").innerHTML = "Chord name 1 will be displayed here";
-    document.getElementById("chordName1").innerHTML = "Chord name 2 will be displayed here";
     synth.releaseAll();
-    //synth.dispose();
   }
 }
 
-function saveConfig() {
-  settings.chordsPerProg = document.getElementById("chordsPerProg").value;
-  settings.minimumChordalMembers= document.getElementById("minimumChordalMembers").value;
-  settings.noteDuration = document.getElementById("noteDuration").value;
-  settings.key = document.getElementById("keySelect").value;
-  settings.loopPlayback = document.getElementById("noteDuration").value === "on" ? true : false;
+function determineChordalMembersAndNames() {
+  currentProgNames = [];
+  for (let i = 0; i < currentProg.length; i++) {
+    var name = hashedReverseDict[getPseudoHash(currentProg[i])] || "<unk>";    
+    currentProgNames.push(name);
+  }
+
+  currentProgMembers = []
+  for (let i = 0; i < currentProgNames.length; i++) {
+    if (currentProgNames[i] === "<unk>") {
+      var notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
+      var chord = mask(notes, currentProg[i]);
+      currentProgMembers.push(chord)
+    }
+    else {
+      var chord = PyChordParser.parseChord(currentProgNames[i][0])
+      var notes = chord.getSpelling();
+
+      currentProgMembers.push(notes);
+    }
+  }
+}
+
+function writeProgToHtml() {
+  var tiles = [];
+  for (let i = 0; i < currentProg.length; i++) {
+    var chordTile = makeChordTile(currentProgMembers[i], currentProgNames[i]);
+
+    tiles.push(chordTile);
+
+  }
+
+  var rows = []
+  for (let i=0; i<tiles.length; i+=3){
+    currentLine = tiles.slice(i,i+3);
+
+    var row = document.createElement("div")
+    row.setAttribute("class", "row")
+    currentLine.forEach(a => row.appendChild(a))
+    rows.push(row)
+  }
+
+  node = document.getElementById("progContainer");
+  while (node.firstChild) {
+    node.removeChild(node.firstChild);
+  }
+
+  rows.forEach(
+    a => node.appendChild(a)
+  )
+}
+
+function makeChordTile(members, name) {
+  var topNode = document.createElement("div")
+  topNode.setAttribute("class", "col-md-4")
+
+  var row1 = document.createElement("div")
+  row1.setAttribute("class", "row")
+  var membersDiv = document.createElement("div")
+  membersDiv.setAttribute("class", "column chord-members-col")
+  membersDiv.appendChild(document.createTextNode(members.map(a => a+"\n").toString().replace(/,/g, "")))
+  row1.appendChild(membersDiv);
+
+  var row2 = document.createElement("div")
+  row2.setAttribute("class", "row")
+  var nameDiv = document.createElement("div");
+  nameDiv.setAttribute("class", "column chord-names-col")
+  nameDiv.appendChild(document.createTextNode(name))
+  row2.appendChild(nameDiv);
+  
+  topNode.appendChild(row1);
+  topNode.appendChild(row2);
+
+  return topNode;
 }
